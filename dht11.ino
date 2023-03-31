c1bb5541fde095a359b31ad95a1f7eed8f9d5247
admin <dummy>
2023-03-31 19:02:06 +1100

    adding some comments

--- dht11.ino
+++ dht11.ino
@@ -2,7 +2,6 @@
 #define PULSE_TIMEOUT 0xFFFFFFFF
 
 int sensor = D1;
-int led = D0;
 
 struct SensorReading {
     unsigned char humidityIntegral, humidityDecimal;
@@ -14,20 +13,21 @@ struct SensorReading reading = SensorReading { 0, 0 };
 
 void setup() {
     
-    pinMode(led, OUTPUT);
     pinMode(sensor, INPUT_PULLUP);
 
 }
 
-
-unsigned long pulseInSafe(int pin, int value){
+unsigned long readPulse(int pin, int value){
     
     unsigned long cycles = 0;
     
-    // the pulse isnt happening yet, wait
+    // the pulse isnt happening yet, so just wait
     if(pinReadFast(pin) != value) 
-        pulseInSafe(pin, !value);
+        readPulse(pin, !value);
     
+    // this code needs to be fast and we dont need to know the absolute length of a pulse,
+    // we just need to compare the high pulse in a bit to the low pulse
+    // so hopefully the varying runtimes of these loop cycles average out
     while(pinReadFast(pin) == value){
         if(cycles++ > 10000) {
             return PULSE_TIMEOUT;
@@ -57,24 +57,21 @@ void sendStartSignal(){
 
 bool receiveResponseSignal(){
     bool status = true;
-    status = status && (pulseInSafe(sensor, LOW) != PULSE_TIMEOUT);
-    status = status && (pulseInSafe(sensor, HIGH) != PULSE_TIMEOUT);
+    status = status && (readPulse(sensor, LOW) != PULSE_TIMEOUT);
+    status = status && (readPulse(sensor, HIGH) != PULSE_TIMEOUT);
     return status;
 }
 
-bool readSensor(){
+bool readSensorBase(){
     
     sendStartSignal();
-    if(!receiveResponseSignal()){
-        return false;
-    }
+    if(!receiveResponseSignal()) return false;
     
     unsigned char bytes[5] = {0, 0, 0, 0, 0};
-    
-    noInterrupts();
+
     for(unsigned int i = 0; i < 40; i++){
-        unsigned long lowPulseWidth = pulseInSafe(sensor, LOW);
-        unsigned long highPulseWidth = pulseInSafe(sensor, HIGH);
+        unsigned long lowPulseWidth = readPulse(sensor, LOW);
+        unsigned long highPulseWidth = readPulse(sensor, HIGH);
         
         if(lowPulseWidth == PULSE_TIMEOUT || highPulseWidth == PULSE_TIMEOUT) 
             return false;
@@ -83,15 +80,26 @@ bool readSensor(){
         if(highPulseWidth > lowPulseWidth) 
             bytes[ i / 8 ] |= 1;
     }
-    interrupts();
     
     reading = SensorReading {
         bytes[0], bytes[1],
         bytes[2], bytes[3],
         bytes[4]
     };
-    
+
     return true;
+}
+
+// a wrapper to avoid tedious return logic in the real function
+bool readSensor(){
+
+    // disabling interrupts = polling the sensor more often
+    noInterrupts();
+        bool status = readSensorBase();
+    interrupts();
+
+    return status;
+
 }
 
 
